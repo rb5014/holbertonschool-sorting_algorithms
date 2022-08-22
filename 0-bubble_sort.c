e0bd2dba53bba97b436cb82e53d6c65b3b1a36c4
rb5014 <5014@holbertonschool.com>
2022-08-22 18:51:25 +0200

    0-bubble_sort.c fixed for array NULL

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,30 +1,32 @@
 #include "sort.h"
 /**
- * bubble_sort - sorts an array of integers in ascending order 
+ * bubble_sort - sorts an array of integers in ascending order
  * using the Bubble sort algorithm
- * 
+ *
  * @array: the array we sort
  * @size: the size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i;
-    int tmp;
-    int perm = 1;
+	int tmp;
+	int perm = 1;
 
-    while(perm != 0)
-    {
-        perm = 0;
-        for(i = 0;  i < size - 1; i++)
-        {
-            if (array[i] > array[i + 1])
-            {
-                tmp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = tmp;
-                print_array(array, size);
-                perm = 1;
-            }
-        }
-    }
+	if (array == NULL || size < 2)
+		return;
+	while (perm != 0)
+	{
+		perm = 0;
+		for (i = 0;  i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+				perm = 1;
+			}
+		}
+	}
 }

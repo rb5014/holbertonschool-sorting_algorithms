5dc07e602d4b91594870144797a9ddd86f8e23a5
rb5014 <5014@holbertonschool.com>
2022-08-22 17:04:03 +0200

    ?

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,2 +1,11 @@
+/**
+ * bubble_sort - sorts an array of integers in ascending order 
+ * using the Bubble sort algorithm
+ * 
+ * @array: the array we sort
+ * @size: the size of the array
+ */
 void bubble_sort(int *array, size_t size)
-{}
+{
+	
+}

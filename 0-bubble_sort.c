95f4d8c717c75f8cfffc6bbb8d27d0c99722b56b
rb5014 <5014@holbertonschool.com>
2022-08-22 18:33:22 +0200

    0-bubble_sort.c, 0-O.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,3 +1,4 @@
+#include "sort.h"
 /**
  * bubble_sort - sorts an array of integers in ascending order 
  * using the Bubble sort algorithm
@@ -7,5 +8,23 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	
+	size_t i;
+    int tmp;
+    int perm = 1;
+
+    while(perm != 0)
+    {
+        perm = 0;
+        for(i = 0;  i < size - 1; i++)
+        {
+            if (array[i] > array[i + 1])
+            {
+                tmp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = tmp;
+                print_array(array, size);
+                perm = 1;
+            }
+        }
+    }
 }
--- sort.h
+++ sort.h
@@ -2,6 +2,7 @@
 #define SORT_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 /**
  * struct listint_s - Doubly linked list node
  *

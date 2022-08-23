e02bf3fb0d9242727c0b2d861c3eea1f746bf737
rb5014 <5014@holbertonschool.com>
2022-08-23 19:40:58 +0200

    1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,10 +2,11 @@
 
 /**
  * swap - swap two nodes of a doubly linked list
- * @nodea: pointer to the first node
- * @nodeb: pointer to the second node
+ * @head: head of the list, changed to 2nd node if 1st node is swapped
+ * @a: pointer to the first node
+ * @b: pointer to the second node
  */
-void swap(listint_t *a, listint_t *b)
+void swap(listint_t **head, listint_t *a, listint_t *b)
 {
 	a->next = b->next;
 	b->prev = a->prev;
@@ -13,12 +14,15 @@ void swap(listint_t *a, listint_t *b)
 		b->next->prev = a;
 	if (a->prev)
 		a->prev->next = b;
+	else
+		*head = b;
 	a->prev = b;
 	b->next = a;
-	return;
+	retu
+	rn;
 }
 /**
- * insertion_sort_list -  sorts a doubly linked list of integers 
+ * insertion_sort_list -  sorts a doubly linked list of integers
  * in ascending order using the Insertion sort algorithm
  * using the Bubble sort algorithm
  * @list: doubly linked list to sort
@@ -26,7 +30,8 @@ void swap(listint_t *a, listint_t *b)
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *tmp, *rev;
-	
+	listint_t *head = *list;
+
 	if (list == NULL)
 		return;
 	tmp = (*list)->next;
@@ -35,14 +40,15 @@ void insertion_sort_list(listint_t **list)
 		if (tmp->n < tmp->prev->n)
 		{
 			rev = tmp;
-			while(rev->prev != NULL && rev->prev->n > rev->n)
+			while (rev->prev != NULL && rev->prev->n > rev->n)
 			{
-				printf("before swap\n");
-				swap(rev->prev, rev);
-				printf("after swap\n");
-				
+				if (rev->prev == head)
+					head = rev;
+				swap(list, rev->prev, rev);
+				print_list(*list);
 			}
 		}
 		tmp = tmp->next;
 	}
+	*list = head;
 }

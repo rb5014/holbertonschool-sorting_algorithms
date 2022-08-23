cc586351b27a8c075414f7749447c89512c3dc79
AdelOtmani <5023@holbertonschool.com>
2022-08-23 12:26:59 +0200

    exo 2 selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,2 +1,22 @@
 void selection_sort(int *array, size_t size)
-{}
+{
+	int i, min, j, tmp;
+
+	for (i = 0; i < (size - 1); i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+			if (min != i)
+			{
+				tmp = array[i];
+				array[i] = min;
+				array[min] = tmp;
+			}
+		}
+	}
+}

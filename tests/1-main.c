b6867ca2e40acf594e0b69c54dbe597c267c0739
Romain Bayle <5014@holbertonstudents.com>
2023-01-09 18:25:31 +0100

    dir tests

--- tests/1-main.c
+++ tests/1-main.c
@@ -1,4 +1,6 @@
-#include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "../sort.h"
 
 /**
  * create_listint - Creates a doubly linked list from an array of integers
@@ -10,25 +12,25 @@
  */
 listint_t *create_listint(const int *array, size_t size)
 {
-    listint_t *list;
-    listint_t *node;
-    int *tmp;
+	listint_t *list;
+	listint_t *node;
+	int *tmp;
 
-    list = NULL;
-    while (size--)
-    {
-        node = malloc(sizeof(*node));
-        if (!node)
-            return (NULL);
-        tmp = (int *)&node->n;
-        *tmp = array[size];
-        node->next = list;
-        node->prev = NULL;
-        list = node;
-        if (list->next)
-            list->next->prev = list;
-    }
-    return (list);
+	list = NULL;
+	while (size--)
+	{
+		node = malloc(sizeof(*node));
+		if (!node)
+			return (NULL);
+		tmp = (int *)&node->n;
+		*tmp = array[size];
+		node->next = list;
+		node->prev = NULL;
+		list = node;
+		if (list->next)
+			list->next->prev = list;
+	}
+	return (list);
 }
 
 /**
@@ -39,7 +41,7 @@ listint_t *create_listint(const int *array, size_t size)
 int main(void)
 {
     listint_t *list;
-    int array[] = {48, 19, 99, 71, 13, 52, 96, 73, 86, 7};
+    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
     size_t n = sizeof(array) / sizeof(array[0]);
 
     list = create_listint(array, n);
